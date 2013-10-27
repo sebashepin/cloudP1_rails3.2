4517abf4a652a83404a907a5b0bc6c9ca28b4bb6
Leslie Solorzano <le.solorzano10@uniandes.edu.co>
2013-10-27 16:24:05 +0000

    elasticache directly called from controllers and helper controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,4 @@
+require 'dalli'
 class SessionsController < ApplicationController
 
   def new
@@ -21,6 +22,9 @@ class SessionsController < ApplicationController
   end
   
   def sign_out
+    id=self.current_user.id
+    dallic = Dalli::Client.new('sessionvm.0e6avx.0001.use1.cache.amazonaws.com:11211', options)
+    dallic.delete('remember_token' + remember_token)
     self.current_user = nil
     cookies.delete(:remember_token)
   end
