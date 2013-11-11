0fd8d31cb049d9ccf6d08cc3fa78933dbedf9bfd
Leslie Solorzano <le.solorzano10@uniandes.edu.co>
2013-11-11 15:47:59 -0500

    remove unused initialiers

--- config/initializers/delayed_jobs_config.rb
+++
@@ -1,5 +0,0 @@
-Delayed::Worker.configure do |config|
-# optional params:
-	config.aws_config = 'config/aws.yml' # Specify the file location of the AWS configuration YAML if you're not using Rails and you want to use a YAML file instead of calling AWS.config
-	config.default_queue_name = 'queue-videocloud' # Specify an alternative default queue name
-end
