e1596670f7a4c5cb43c20556180de422071ccecc
Leslie Solorzano <le.solorzano10@uniandes.edu.co>
2013-11-12 01:08:14 -0500

    changed controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,15 +51,15 @@ class UsersController < ApplicationController
     @uvideo.user_id = params[:user_id]
     @uvideo.estado =  Video::PROCESSING_STATE
     uploaded_io = params[:file]
-    path = Rails.root.join('public', 'uploads', uploaded_io.original_filename)
+    path = "public/uploads/"+uploaded_io.original_filename)
 
     #File.open(path, 'wb:ASCII-8BIT') do |file|
     #  file.write(uploaded_io.read)
     #end
 
-    uploadPath = "public/uploads/" + File.absolute_path(path).split('public/uploads/')[1]
-    @uvideo.file = uploadPath
-    suffix = File.absolute_path(path).split(".")[1]
+    #uploadPath = "public/uploads/" + File.absolute_path(path).split('public/uploads/')[1]
+    #@uvideo.file = uploadPath
+    #suffix = File.absolute_path(path).split(".")[1]
     key = File.basename(path)
     puts "Key"
     puts key
